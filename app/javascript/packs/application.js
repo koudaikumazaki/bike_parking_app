f82aeafd943f52ec219bb4ef9ea1bde4b9f40d7c
koudaikumazaki <kk1974340@gmail.com>
2021-02-09 22:00:22 +0900

    change-ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 require("jquery")
+require("packs/hamburger_menu")
 
 
 
---
+++ app/javascript/packs/hamburger_menu.js
@@ -0,0 +1,30 @@
+'use strict'; 
+{
+  document.addEventListener("DOMContentLoaded", () => {
+    
+    document.addEventListener("turbolinks:load", () => {
+      const open = document.getElementsByClassName('open')[0];
+      const close = document.getElementsByClassName('close')[0];
+      const sp_navi = document.getElementsByClassName('sp_navi')[0];
+      open.addEventListener('click', () => {
+        sp_navi.classList.add('show');
+        open.classList.add('hide');
+      });
+    
+      close.addEventListener('click', () => {
+        sp_navi.classList.remove('show');
+        open.classList.remove('hide');
+      });
+    });
+  });
+  //   open.addEventListener('click', () => {
+  //     sp_navi.classList.add('show');
+  //     open.classList.add('hide');
+  //   });
+  
+  //   close.addEventListener('click', () => {
+  //     sp_navi.classList.remove('show');
+  //     open.classList.remove('hide');
+  //   });
+  // });
+}
