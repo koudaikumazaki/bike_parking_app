178e2be7e6731c47489f444a92729bc4f183ed5d
koudaikumazaki <kk1974340@gmail.com>
2021-02-13 22:57:39 +0900

    add-search-function

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV['GOOGLE_API_KEY'] # API key for geocoding service
+  api_key: ENV['GOOGLE_API_KEY'], # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
@@ -17,6 +17,6 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
+  units: :km            # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
+  get 'search', to: 'parkings#search'
+
 end
