0e35940a8a656c4fe61a137c78400cd5dc6f16e3
koudaikumazaki <kk1974340@gmail.com>
2021-03-13 16:44:38 +0900

    default_scopeを修正

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -27,7 +27,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Parking < ApplicationRecord
-  default_scope { order("updated_at ASC") }
+  default_scope { order("updated_at DESC") }
   attr_accessor :distance
   mount_uploader :image, ImageUploader
   belongs_to :user
