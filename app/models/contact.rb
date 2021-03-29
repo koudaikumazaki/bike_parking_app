24595475392da37b3dd919889f5e9a54f8e068a6
koudaikumazaki <kk1974340@gmail.com>
2021-03-29 14:01:35 +0900

    モデルにバリデーションを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,4 +12,10 @@
 #  updated_at :datetime         not null
 #
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
+  validates :genre, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 500 }
 end
