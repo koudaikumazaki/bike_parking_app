c9769a3e575a8742ceec5ab950ab24a4ed2a3d1a
koudaikumazaki <kk1974340@gmail.com>
2022-01-08 15:26:15 +0900

    fix: registar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ module Users
     before_action :check_guest, only: [:update, :destroy]
 
     def check_guest
-      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。' if resource.email == User::GUEST_EMAIL
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。' if resource.email == ENV['GUEST_EMAIL']
     end
 
   end
