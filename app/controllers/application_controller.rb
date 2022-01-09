161145f53b817a2922c30d0aa2c60507f9a7b080
koudaikumazaki <kk1974340@gmail.com>
2022-01-09 09:58:06 +0900

    refactor: application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # FIXME: まとめられそう。。
   def configure_permitted_parameters
     added_attrs = [:email, :username, :password, :password_confirmation]
+
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
