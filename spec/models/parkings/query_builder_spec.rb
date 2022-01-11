d6e5f4b70463e14c90f321a33d15bb80f41680fe
koudaikumazaki <kk1974340@gmail.com>
2022-01-11 15:36:36 +0900

    clean: comment

--- spec/models/parkings/query_builder_spec.rb
+++ spec/models/parkings/query_builder_spec.rb
@@ -9,14 +9,6 @@ RSpec.describe Parkings::QueryBuilder, type: :model do
       it { is_expected.to be_empty }
     end
     context "keyword is near" do
-      let(:query_builder) { Parkings::QueryBuilder.new(location: '東京駅', keyword: 'near') }
-      let(:parking) { create(:parking) }
-      let(:other_parking) { create(:parking, latitude: '35.80') }
-      subject { query_builder.search }
-      before do
-        query_builder.
-      end
-      it { is_expected.to match_array([parking, other_parking]) }
     end
     context "keyword is inexpensive" do
       
